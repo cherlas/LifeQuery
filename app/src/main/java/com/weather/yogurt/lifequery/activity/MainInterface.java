33a072ded31d5f65b1f63bf50b0558f1ad03b348
Git-Yogurt <vm@li.cm>
2016-06-30 09:31:38 +0800

    finished the spinner view choose item

--- app/src/main/java/com/weather/yogurt/lifequery/activity/MainInterface.java
+++ app/src/main/java/com/weather/yogurt/lifequery/activity/MainInterface.java
@@ -1,15 +1,29 @@
 package com.weather.yogurt.lifequery.activity;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.res.Resources;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 import com.weather.yogurt.lifequery.R;
 
-public class MainInterface extends AppCompatActivity {
+import java.util.Arrays;
+import java.util.List;
 
+public class MainInterface extends AppCompatActivity {
+    private Spinner spinnerChooseItem;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main_interface);
+        spinnerChooseItem= (Spinner) findViewById(R.id.spinner_choose_item);
+        //spinner drop down data;
+        Resources resources=getResources();
+        List<String> spinnerDropDownData= Arrays.asList(resources.getStringArray(R.array.spinner_drop_down_item));
+        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this,android.R.layout.simple_spinner_item,spinnerDropDownData);
+        arrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinnerChooseItem.setAdapter(arrayAdapter);
     }
 }
