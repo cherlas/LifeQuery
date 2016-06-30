67ae6920806e1b09847b1c61112d18777791c85d
Git-Yogurt <vm@li.cm>
2016-06-30 08:51:35 +0800

    Rename the main interface files

--- app/src/main/java/com/weather/yogurt/lifequery/activity/MainInterface.java
+++ app/src/main/java/com/weather/yogurt/lifequery/activity/MainInterface.java
@@ -1,13 +1,15 @@
-package com.weather.yogurt.lifequery;
+package com.weather.yogurt.lifequery.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.weather.yogurt.lifequery.R;
+
+public class MainInterface extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.main_interface);
     }
 }
