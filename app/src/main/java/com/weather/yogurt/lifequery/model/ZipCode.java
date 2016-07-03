8d6ceba46930217527b9e1c3721a39dad9fd8f3d
Git-Yogurt <vm@li.cm>
2016-07-03 15:50:36 +0800

    add ZipCode model

---
+++ app/src/main/java/com/weather/yogurt/lifequery/model/ZipCode.java
@@ -0,0 +1,25 @@
+package com.weather.yogurt.lifequery.model;
+
+/**
+ * Created by Yogurt on 16/7/3.
+ */
+public class ZipCode {
+    private String zipCodeNumber;
+    private String searchDate;
+
+    public String getZipCodeNumber() {
+        return zipCodeNumber;
+    }
+
+    public void setZipCodeNumber(String zipCodeNumber) {
+        this.zipCodeNumber = zipCodeNumber;
+    }
+
+    public String getSearchDate() {
+        return searchDate;
+    }
+
+    public void setSearchDate(String searchDate) {
+        this.searchDate = searchDate;
+    }
+}
