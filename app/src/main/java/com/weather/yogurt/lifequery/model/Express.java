f3779523291407b570e62ed552a1cb4fa59ba504
Git-Yogurt <vm@li.cm>
2016-07-03 15:42:40 +0800

    add Express model

---
+++ app/src/main/java/com/weather/yogurt/lifequery/model/Express.java
@@ -0,0 +1,27 @@
+package com.weather.yogurt.lifequery.model;
+
+/**
+ * Created by Yogurt on 16/7/3.
+ * 快递
+ */
+public class Express {
+    private String express;
+
+    public String getSearchDate() {
+        return searchDate;
+    }
+
+    public void setSearchDate(String searchDate) {
+        this.searchDate = searchDate;
+    }
+
+    public String getExpress() {
+        return express;
+    }
+
+    public void setExpress(String express) {
+        this.express = express;
+    }
+
+    private String searchDate;
+}
