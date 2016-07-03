6d147215376f2657f9f00dc17eac3c184951b804
Git-Yogurt <vm@li.cm>
2016-07-03 15:52:48 +0800

    add exchangerate model

---
+++ app/src/main/java/com/weather/yogurt/lifequery/model/ExchangeRate.java
@@ -0,0 +1,25 @@
+package com.weather.yogurt.lifequery.model;
+
+/**
+ * Created by Yogurt on 16/7/3.
+ */
+public class ExchangeRate {
+    private String exchangeRate;
+    private String searchDate;
+
+    public String getExchangeRate() {
+        return exchangeRate;
+    }
+
+    public void setExchangeRate(String exchangeRate) {
+        this.exchangeRate = exchangeRate;
+    }
+
+    public String getSearchDate() {
+        return searchDate;
+    }
+
+    public void setSearchDate(String searchDate) {
+        this.searchDate = searchDate;
+    }
+}
