476988034e4afc7cdc297a463f4153726067f3ec
Git-Yogurt <vm@li.cm>
2016-07-03 16:18:17 +0800

    add Trademark model

---
+++ app/src/main/java/com/weather/yogurt/lifequery/model/Trademark.java
@@ -0,0 +1,25 @@
+package com.weather.yogurt.lifequery.model;
+
+/**
+ * Created by Yogurt on 16/7/3.
+ */
+public class Trademark {
+    private String trademark;
+    private String searchDate;
+
+    public String getTrademark() {
+        return trademark;
+    }
+
+    public void setTrademark(String trademark) {
+        this.trademark = trademark;
+    }
+
+    public String getSearchDate() {
+        return searchDate;
+    }
+
+    public void setSearchDate(String searchDate) {
+        this.searchDate = searchDate;
+    }
+}
