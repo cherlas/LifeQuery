bf3d908f397fc690ad0709dfaff5d5dec498a57d
Git-Yogurt <vm@li.cm>
2016-07-03 15:38:37 +0800

    add AppleSerialNumber model

---
+++ app/src/main/java/com/weather/yogurt/lifequery/model/AppleSerialNumber.java
@@ -0,0 +1,25 @@
+package com.weather.yogurt.lifequery.model;
+
+/**
+ * Created by Yogurt on 16/7/3.
+ */
+public class AppleSerialNumber {
+    private String appleSerialNumber;
+    private String searchDate;
+
+    public String getAppleSerialNumber() {
+        return appleSerialNumber;
+    }
+
+    public void setAppleSerialNumber(String appleSerialNumber) {
+        this.appleSerialNumber = appleSerialNumber;
+    }
+
+    public String getSearchDate() {
+        return searchDate;
+    }
+
+    public void setSearchDate(String searchDate) {
+        this.searchDate = searchDate;
+    }
+}
