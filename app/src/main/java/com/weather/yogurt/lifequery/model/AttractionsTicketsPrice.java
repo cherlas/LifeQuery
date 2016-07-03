4c57d9b4c79425bdd1082404f85c100d356380f4
Git-Yogurt <vm@li.cm>
2016-07-03 16:19:49 +0800

    add AttractionsTickets model

---
+++ app/src/main/java/com/weather/yogurt/lifequery/model/AttractionsTicketsPrice.java
@@ -0,0 +1,25 @@
+package com.weather.yogurt.lifequery.model;
+
+/**
+ * Created by Yogurt on 16/7/3.
+ */
+public class AttractionsTicketsPrice {
+    private String attractionName;
+    private String searchDate;
+
+    public String getAttractionName() {
+        return attractionName;
+    }
+
+    public void setAttractionName(String attractionName) {
+        this.attractionName = attractionName;
+    }
+
+    public String getSearchDate() {
+        return searchDate;
+    }
+
+    public void setSearchDate(String searchDate) {
+        this.searchDate = searchDate;
+    }
+}
