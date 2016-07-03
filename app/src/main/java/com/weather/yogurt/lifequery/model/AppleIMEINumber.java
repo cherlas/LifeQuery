59e0ce218ecfc9e0965c589de969d1d09e6e07c5
Git-Yogurt <vm@li.cm>
2016-07-03 15:41:29 +0800

    add AppleIMEINumber model

---
+++ app/src/main/java/com/weather/yogurt/lifequery/model/AppleIMEINumber.java
@@ -0,0 +1,25 @@
+package com.weather.yogurt.lifequery.model;
+
+/**
+ * Created by Yogurt on 16/7/3.
+ */
+public class AppleIMEINumber {
+    public String getAppleIMEINumber() {
+        return appleIMEINumber;
+    }
+
+    public void setAppleIMEINumber(String appleIMEINumber) {
+        this.appleIMEINumber = appleIMEINumber;
+    }
+
+    public String getSearchDate() {
+        return searchDate;
+    }
+
+    public void setSearchDate(String searchDate) {
+        this.searchDate = searchDate;
+    }
+
+    private String appleIMEINumber;
+    private String searchDate;
+}
