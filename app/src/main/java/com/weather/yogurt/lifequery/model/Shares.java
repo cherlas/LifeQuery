d10912e76674a34c9cbd9365338a80fe02ff784d
Git-Yogurt <vm@li.cm>
2016-07-03 15:48:10 +0800

    add shares model

---
+++ app/src/main/java/com/weather/yogurt/lifequery/model/Shares.java
@@ -0,0 +1,27 @@
+package com.weather.yogurt.lifequery.model;
+
+/**
+ * Created by Yogurt on 16/7/3.
+ * 股票
+ */
+public class Shares {
+    private String shares;
+
+    public String getSearchDate() {
+        return searchDate;
+    }
+
+    public void setSearchDate(String searchDate) {
+        this.searchDate = searchDate;
+    }
+
+    public String getShares() {
+        return shares;
+    }
+
+    public void setShares(String shares) {
+        this.shares = shares;
+    }
+
+    private String searchDate;
+}
