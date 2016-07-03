40f2656248cd21069a4b7f59c88dd966a6acfde4
Git-Yogurt <vm@li.cm>
2016-07-03 16:22:34 +0800

    add PerpetualCalendar model

---
+++ app/src/main/java/com/weather/yogurt/lifequery/model/PerpetualCalendar.java
@@ -0,0 +1,25 @@
+package com.weather.yogurt.lifequery.model;
+
+/**
+ * Created by Yogurt on 16/7/3.
+ */
+public class PerpetualCalendar {
+    private String perpetualCalendar;
+    private String searchDate;
+
+    public String getPerpetualCalendar() {
+        return perpetualCalendar;
+    }
+
+    public void setPerpetualCalendar(String perpetualCalendar) {
+        this.perpetualCalendar = perpetualCalendar;
+    }
+
+    public String getSearchDate() {
+        return searchDate;
+    }
+
+    public void setSearchDate(String searchDate) {
+        this.searchDate = searchDate;
+    }
+}
