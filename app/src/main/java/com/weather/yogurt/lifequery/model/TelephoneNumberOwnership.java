30cb721442acb9751ae70400840056f35fac4d08
Git-Yogurt <vm@li.cm>
2016-07-03 15:32:53 +0800

    add TelephoneNumberOwnership model

---
+++ app/src/main/java/com/weather/yogurt/lifequery/model/TelephoneNumberOwnership.java
@@ -0,0 +1,26 @@
+package com.weather.yogurt.lifequery.model;
+
+/**
+ * Created by Yogurt on 16/7/3.
+ */
+public class TelephoneNumberOwnership {
+    private String telephoneNumber;
+    private String search_date;
+    public String getSearch_date() {
+        return search_date;
+    }
+
+    public void setSearch_date(String search_date) {
+        this.search_date = search_date;
+    }
+
+    public String getTelephoneNumber() {
+        return telephoneNumber;
+    }
+
+    public void setTelephoneNumber(String telephoneNumber) {
+        this.telephoneNumber = telephoneNumber;
+    }
+
+
+}
