e15ce39905ac41aa5896797ed382c4ac85ade850
Git-Yogurt <vm@li.cm>
2016-07-08 15:38:22 +0800

    更新搜索按钮点击事件

--- app/src/main/java/com/weather/yogurt/lifequery/activity/MainInterface.java
+++ app/src/main/java/com/weather/yogurt/lifequery/activity/MainInterface.java
@@ -1,5 +1,6 @@
 package com.weather.yogurt.lifequery.activity;
 
+import android.content.Intent;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -10,8 +11,6 @@ import android.widget.ImageButton;
 import android.widget.Spinner;
 
 import com.weather.yogurt.lifequery.R;
-import com.weather.yogurt.lifequery.util.HttpCallbackListener;
-import com.weather.yogurt.lifequery.util.HttpUtil;
 
 import java.util.Arrays;
 import java.util.List;
@@ -43,66 +42,16 @@ public class MainInterface extends AppCompatActivity implements View.OnClickList
     public void onClick(View v) {
         switch (v.getId()){
             case R.id.search_button:
-                String chooseItem= (String) spinnerChooseItem.getSelectedItem();
-                String inputContent= String.valueOf(searchContent.getText());
-
-                HttpCallbackListener listener=new HttpCallbackListener() {
-                    @Override
-                    public void onFinish(String result) {
-
-                    }
-
-                    @Override
-                    public void onError(Exception e) {
-
-                    }
-                };
-                String searchAddress="";
-                switch (chooseItem){
-                    case "电话归属地": //http://apistore.baidu.com/apiworks/servicedetail/794.html
-                        searchAddress="http://apis.baidu.com/apistore/mobilenumber/mobilenumber?"+inputContent;
-                        break;
-                    case "IP地址": //http://apistore.baidu.com/apiworks/servicedetail/114.html
-                        searchAddress="http://apis.baidu.com/apistore/iplookupservice/iplookup?"+inputContent;
-                        break;
-                    case "邮编"://http://avatardata.cn/Docs/Api/b3d25cbd-449d-41c3-8765-21649658789e
-                        searchAddress="http://api.avatardata.cn/PostNumber/QueryPostnumber?key=[您申请的APPKEY]&postnumber="
-                                +inputContent+"&page=1&rows=15?";
-                        break;
-                    case "银行卡"://http://apistore.baidu.com/apiworks/servicedetail/735.html
-                        searchAddress="http://apis.baidu.com/datatiny/cardinfo/cardinfo"+inputContent;
-                        break;
-                    case "苹果序列号"://...
-                        searchAddress="http://apis.baidu.com/apistore/mobilenumber/mobilenumber?"+inputContent;
-                        break;
-                    case "苹果IMEI"://...
-                        searchAddress="http://apis.baidu.com/apistore/mobilenumber/mobilenumber?"+inputContent;
-                        break;
-                    case "汇率"://http://apistore.baidu.com/apiworks/servicedetail/119.html //界面
-                        searchAddress="http://apis.baidu.com/apistore/currencyservice/currency?"+"fromCurrency=CNY&toCurrency=USD&amount=2";
-                        break;
-                    case "快递":
-                        searchAddress="http://apis.baidu.com/apistore/mobilenumber/mobilenumber?"+inputContent;
-                        break;
-                    case "股票":
-                        searchAddress="http://apis.baidu.com/apistore/mobilenumber/mobilenumber?"+inputContent;
-                        break;
-                    case "火车票":
-                        searchAddress="http://apis.baidu.com/apistore/mobilenumber/mobilenumber?"+inputContent;
-                        break;
-                    case "商标":
-                        searchAddress="http://apis.baidu.com/apistore/mobilenumber/mobilenumber?"+inputContent;
-                        break;
-                    case "景点门票":
-                        searchAddress="http://apis.baidu.com/apistore/mobilenumber/mobilenumber?"+inputContent;
-                        break;
-                    case "万年历":
-                        searchAddress="http://apis.baidu.com/apistore/mobilenumber/mobilenumber?"+inputContent;
-                        break;
-                }
-                HttpUtil.sendHttpRequest(searchAddress,listener);
+                final String chooseItem= (String) spinnerChooseItem.getSelectedItem();
+                final String inputContent= String.valueOf(searchContent.getText());
+                Intent showInformationIntent=new Intent(MainInterface.this,ShowInformationActivity.class);
+                showInformationIntent.putExtra("isFromMainInterface",true);
+                showInformationIntent.putExtra("chooseItem",chooseItem);
+                showInformationIntent.putExtra("inputContent",inputContent);
+                startActivity(showInformationIntent);
                 break;
-            default:break;
+            default:
+                break;
         }
 
     }
