1f3a5c9190d09589744ecbaffc0c5da707000b28
Git-Yogurt <vm@li.cm>
2016-07-08 17:23:57 +0800

    edit httpcallbacklistener.java

--- app/src/main/java/com/weather/yogurt/lifequery/util/HttpCallbackListener.java
+++ app/src/main/java/com/weather/yogurt/lifequery/util/HttpCallbackListener.java
@@ -1,9 +1,11 @@
 package com.weather.yogurt.lifequery.util;
 
+import org.json.JSONException;
+
 /**
  * Created by Yogurt on 16/7/7.
  */
 public interface HttpCallbackListener {
-    void onFinish(String result);
+    void onFinish(String result) throws JSONException;
     void onError(Exception e);
 }
