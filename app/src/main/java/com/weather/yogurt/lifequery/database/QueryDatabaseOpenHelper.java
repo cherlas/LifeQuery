20ae5678917a9bbae4cd6d63f7cd05280be36e3e
Git-Yogurt <vm@li.cm>
2016-07-11 19:47:58 +0800

    edit create table sentence

--- app/src/main/java/com/weather/yogurt/lifequery/database/QueryDatabaseOpenHelper.java
+++ app/src/main/java/com/weather/yogurt/lifequery/database/QueryDatabaseOpenHelper.java
@@ -14,91 +14,91 @@ public class QueryDatabaseOpenHelper extends SQLiteOpenHelper{
      */
     public static final String CREATE_TELEPHONE_HOME_OWNERSHIP="create table TelephoneHomeOwnership("
             +"id integer primary key autoincrement,"
-            +"telephone_number text"
+            +"telephone_number text,"
             +"telephone_search_date text)";
     /*
       IP地址建表语句
     */
     public static final String CREATE_IP_ADDRESS="create table IpAddress("
             +"id integer primary key autoincrement,"
-            +"ip_address text"
+            +"ip_address text,"
             +"ip_address_search_date text)";
     /*
       Apple序列号建表语句
     */
     public static final String CREATE_APPLE_SERIAL_NUMBER="create table AppleSerialNumber("
             +"id integer primary key autoincrement,"
-            +"apple_serial_number text"
+            +"apple_serial_number text,"
             +"apple_serial_number_search_date text)";
     /*
       AppleIMEI建表语句
     */
     public static final String CREATE_APPLE_IMEI_NUMBER="create table AppleIMEINumber("
             +"id integer primary key autoincrement,"
-            +"apple_imei_number text"
+            +"apple_imei_number text,"
             +"apple_imei_number_search_date text)";
     /*
       快递建表语句
     */
     public static final String CREATE_EXPRESS="create table Express("
             +"id integer primary key autoincrement,"
-            +"express_number text"
+            +"express_number text,"
             +"express_search_date text)";
     /*
       银行卡建表语句
     */
     public static final String CREATE_BANK_CARD="create table BankCard("
             +"id integer primary key autoincrement,"
-            +"bank_card_number text"
+            +"bank_card_number text,"
             +"bank_card_search_date text)";
     /*
       股票建表语句
     */
     public static final String CREATE_SHARES="create table Shares("
             +"id integer primary key autoincrement,"
-            +"shares text"
+            +"shares text,"
             +"shares_search_date text)";
     /*
       火车票建表语句
     */
     public static final String CREATE_TRAIN_TICKETS="create table TrainTickets("
             +"id integer primary key autoincrement,"
-            +"train_tickets_number text"
+            +"train_tickets_number text,"
             +"train_tickets_search_date text)";
     /*
       邮编建表语句
     */
     public static final String CREATE_ZIP_CODE="create table ZipCode("
             +"id integer primary key autoincrement,"
-            +"zip_code_number text"
+            +"zip_code_number text,"
             +"zip_code_search_date text)";
     /*
       汇率建表语句
     */
     public static final String CREATE_EXCHANGE_RATE="create table ExchangeRate("
             +"id integer primary key autoincrement,"
-            +"exchange_rate text"
+            +"exchange_rate text,"
             +"exchange_rate_search_date text)";
     /*
       商标建表语句
     */
     public static final String CREATE_TRADEMARK="create table Trademark("
             +"id integer primary key autoincrement,"
-            +"trademark text"
+            +"trademark text,"
             +"trademark_search_date text)";
     /*
       景点名票建表语句 去哪儿网
     */
     public static final String CREATE_ATTRACTIONSTICKETS_PRICE="create table AttractionsTickets("
             +"id integer primary key autoincrement,"
-            +"attractions_name text"
+            +"attractions_name text,"
             +"attractions_search_date text)";
     /*
       万年历建表语句
     */
     public static final String CREATE_PERPETUAL_CALENDAR="create table PerpetualCalendar("
             +"id integer primary key autoincrement,"
-            +"perpetual_calendar text"
+            +"perpetual_calendar text,"
             +"perpetual_calendar_search_date text)";
 
 
