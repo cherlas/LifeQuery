09f777d8f0e00c786d441a91243396fbab351dcb
Git-Yogurt <vm@li.cm>
2016-07-13 10:46:00 +0800

    edit parse methods

--- app/src/main/java/com/weather/yogurt/lifequery/util/Utilty.java
+++ app/src/main/java/com/weather/yogurt/lifequery/util/Utilty.java
@@ -4,65 +4,77 @@ import android.content.Context;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
 
+import com.weather.yogurt.lifequery.database.QueryDataBase;
+import com.weather.yogurt.lifequery.model.TelephoneNumberOwnership;
+
 import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 /**
  * Created by Yogurt on 16/7/7.
  */
 public class Utilty {
 
-    public static boolean parseShares(Context context,JSONObject object) {
+    private static SimpleDateFormat format=new SimpleDateFormat("MM-dd HH:mm");
+
+    public static boolean parseShares(Context context, JSONObject object, String inputContent, QueryDataBase dataBase) {
         return false;
     }
 
-    public static boolean parseExpress(Context context,JSONObject object) {
+    public static boolean parseExpress(Context context, JSONObject object, String inputContent, QueryDataBase dataBase) {
         return false;
     }
 
-    public static boolean parseTrainTickets(Context context,JSONObject object) {
+    public static boolean parseTrainTickets(Context context, JSONObject object, String inputContent, QueryDataBase dataBase) {
         return false;
     }
 
-    public static boolean parseTrademark(Context context,JSONObject object) {
+    public static boolean parseTrademark(Context context, JSONObject object, String inputContent, QueryDataBase dataBase) {
         return false;
     }
 
-    public static boolean parseExchangeRate(Context context,JSONObject object) {
+    public static boolean parseExchangeRate(Context context, JSONObject object, String inputContent, QueryDataBase dataBase) {
         return false;
     }
 
-    public static boolean parseAppleIMEINumber(Context context,JSONObject object) {
+    public static boolean parseAppleIMEINumber(Context context, JSONObject object, String inputContent, QueryDataBase dataBase) {
         return false;
     }
 
-    public static boolean parseAttractionsTicketsPrice(Context context,JSONObject object) {
+    public static boolean parseAttractionsTicketsPrice(Context context, JSONObject object, String inputContent, QueryDataBase dataBase) {
         return false;
     }
 
-    public static boolean parsePerpetualCalendar(Context context,JSONObject object) {
+    public static boolean parsePerpetualCalendar(Context context, JSONObject object, String inputContent, QueryDataBase dataBase) {
         return false;
     }
 
-    public static boolean parseAppleSerialNumber(Context context,JSONObject object) {
+    public static boolean parseAppleSerialNumber(Context context, JSONObject object, String inputContent, QueryDataBase dataBase) {
         return false;
     }
 
-    public static boolean parseBankCard(Context context,JSONObject object) {
+    public static boolean parseBankCard(Context context, JSONObject object, String inputContent, QueryDataBase dataBase) {
         return false;
     }
 
-    public static boolean parseZipCode(Context context,JSONObject object) {
+    public static boolean parseZipCode(Context context, JSONObject object, String inputContent, QueryDataBase dataBase) {
         return false;
     }
 
-    public static boolean parseIpAddress(Context context,JSONObject object) {
+    public static boolean parseIpAddress(Context context, JSONObject object, String inputContent, QueryDataBase dataBase) {
         return false;
     }
 
-    public static boolean parsePhoneNumberOwnership(Context context, JSONObject object) throws JSONException {
+    public static boolean parsePhoneNumberOwnership(Context context, JSONObject object, String inputContent,QueryDataBase dataBase) throws JSONException {
         String errNum=object.getString("errNum");
         if (errNum.equals("0")){
+            TelephoneNumberOwnership ownership=new TelephoneNumberOwnership();
+            ownership.setTelephoneNumber(inputContent);
+            ownership.setSearchDate(format.format(new Date()));
+            dataBase.saveTelephoneNumberOwnershipInformation(ownership);
             if (object.getString("retMsg").equals("success")){
                 JSONObject retData=object.getJSONObject("retData");
                 SharedPreferences.Editor editor= PreferenceManager.getDefaultSharedPreferences(context).edit();
