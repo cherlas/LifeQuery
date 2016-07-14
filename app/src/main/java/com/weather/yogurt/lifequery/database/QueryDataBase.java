b805ab178efb841b834378bb30fcf8d9a07e8cbe
Git-Yogurt <vm@li.cm>
2016-07-14 09:57:28 +0800

    更新save方法

--- app/src/main/java/com/weather/yogurt/lifequery/database/QueryDataBase.java
+++ app/src/main/java/com/weather/yogurt/lifequery/database/QueryDataBase.java
@@ -70,128 +70,212 @@ public class QueryDataBase {
         }
     }
 
-    private Cursor query(String tableName, String telephoneNumber) {
-        Cursor cursor=db.query(tableName,new String[]{telephoneNumber},null,null,null,null,null);
+    private Cursor query(String tableName, String searchNumber) {
+        Cursor cursor=db.query(tableName,new String[]{searchNumber},null,null,null,null,null);
         return cursor;
     }
 
     //ip地址
     public void saveIpAddressInformation(IPAddress ipAddress){
         if (ipAddress!=null){
-            ContentValues values=new ContentValues();
-            values.put("ip_address",ipAddress.getIpAddress());
-            values.put("ip_address_search_date",ipAddress.getSearchDate());
-            db.insert("IpAddress",null,values);
+            Cursor cursor=query("IpAddress",ipAddress.getIpAddress());
+            if (cursor.getCount()!=0){
+                ContentValues values=new ContentValues();
+                values.put("ip_address_search_date",ipAddress.getSearchDate());
+                db.update("IpAddress",values,"ip_address=?",new String[]{ipAddress.getIpAddress()});
+            }else {
+                ContentValues values=new ContentValues();
+                values.put("telephone_number",ipAddress.getIpAddress());
+                values.put("telephone_search_date",ipAddress.getSearchDate());
+                db.insert("IpAddress",null,values);
+            }
         }
     }
 
     //AppleSerialNumber
     public void saveAppleSerialNumberInformation(AppleSerialNumber appleSerialNumber){
         if (appleSerialNumber!=null){
-            ContentValues values=new ContentValues();
-            values.put("apple_serial_number",appleSerialNumber.getAppleSerialNumber());
-            values.put("apple_serial_number_search_date",appleSerialNumber.getSearchDate());
-            db.insert("AppleSerialNumber",null,values);
+            Cursor cursor=query("AppleSerialNumber",appleSerialNumber.getAppleSerialNumber());
+            if (cursor.getCount()!=0){
+                ContentValues values=new ContentValues();
+                values.put("apple_serial_number_search_date",appleSerialNumber.getSearchDate());
+                db.update("AppleSerialNumber",values,"apple_serial_number=?",new String[]{appleSerialNumber.getAppleSerialNumber()});
+            }else {
+                ContentValues values=new ContentValues();
+                values.put("apple_serial_number",appleSerialNumber.getAppleSerialNumber());
+                values.put("apple_serial_number_search_date",appleSerialNumber.getSearchDate());
+                db.insert("AppleSerialNumber",null,values);
+            }
         }
     }
 
     //Apple IMEI
     public void saveAppleIMEINumberInformation(AppleIMEINumber appleIMEINumber){
         if (appleIMEINumber!=null){
-            ContentValues values=new ContentValues();
-            values.put("apple_imei_number",appleIMEINumber.getAppleIMEINumber());
-            values.put("apple_imei_number_search_date",appleIMEINumber.getSearchDate());
-            db.insert("AppleIMEINumber",null,values);
+            Cursor cursor=query("AppleIMEINumber",appleIMEINumber.getAppleIMEINumber());
+            if (cursor.getCount()!=0){
+                ContentValues values=new ContentValues();
+                values.put("apple_imei_number_search_date",appleIMEINumber.getSearchDate());
+                db.update("AppleIMEINumber",values,"apple_imei_number=?",new String[]{appleIMEINumber.getAppleIMEINumber()});
+            }else {
+                ContentValues values=new ContentValues();
+                values.put("apple_imei_number",appleIMEINumber.getAppleIMEINumber());
+                values.put("apple_imei_number_search_date",appleIMEINumber.getSearchDate());
+                db.insert("AppleIMEINumber",null,values);
+            }
         }
     }
 
     //Express
     public void saveExpressInformation(Express express){
         if (express!=null){
-            ContentValues values=new ContentValues();
-            values.put("express_number",express.getExpress());
-            values.put("express_search_date",express.getSearchDate());
-            db.insert("Express",null,values);
+            Cursor cursor=query("Express",express.getExpress());
+            if (cursor.getCount()!=0){
+                ContentValues values=new ContentValues();
+                values.put("express_search_date",express.getSearchDate());
+                db.update("Express",values,"express_number=?",new String[]{express.getExpress()});
+            }else {
+                ContentValues values=new ContentValues();
+                values.put("express_number",express.getExpress());
+                values.put("express_search_date",express.getSearchDate());
+                db.insert("Express",null,values);
+            }
         }
     }
 
     //BankCard
     public void saveBankCardInformation(BankCard bankCard){
         if (bankCard!=null){
-            ContentValues values=new ContentValues();
-            values.put("bank_card_number",bankCard.getBankCardNumber());
-            values.put("bank_card_search_date",bankCard.getSearchDate());
-            db.insert("BankCard",null,values);
+            Cursor cursor=query("BankCard",bankCard.getBankCardNumber());
+            if (cursor.getCount()!=0){
+                ContentValues values=new ContentValues();
+                values.put("bank_card_search_date",bankCard.getSearchDate());
+                db.update("BankCard",values,"bank_card_number=?",new String[]{bankCard.getBankCardNumber()});
+            }else {
+                ContentValues values=new ContentValues();
+                values.put("bank_card_number",bankCard.getBankCardNumber());
+                values.put("bank_card_search_date",bankCard.getSearchDate());
+                db.insert("BankCard",null,values);
+            }
         }
     }
 
     //Shares
     public void saveSharesInformation(Shares shares){
         if (shares!=null){
-            ContentValues values=new ContentValues();
-            values.put("shares",shares.getShares());
-            values.put("shares_search_date",shares.getSearchDate());
-            db.insert("Shares",null,values);
+            Cursor cursor=query("Shares",shares.getShares());
+            if (cursor.getCount()!=0){
+                ContentValues values=new ContentValues();
+                values.put("shares_search_date",shares.getSearchDate());
+                db.update("Shares",values,"shares=?",new String[]{shares.getShares()});
+            }else {
+                ContentValues values=new ContentValues();
+                values.put("shares",shares.getShares());
+                values.put("shares_search_date",shares.getSearchDate());
+                db.insert("Shares",null,values);
+            }
         }
     }
 
     //TrainTickets
     public void saveTrainTicketsInformation(TrainTickets trainTickets){
         if (trainTickets!=null){
-            ContentValues values=new ContentValues();
-            values.put("train_tickets_number",trainTickets.getTrainTicketsNumber());
-            values.put("train_tickets_search_date",trainTickets.getSearchDate());
-            db.insert("TrainTickets",null,values);
+            Cursor cursor=query("TrainTickets",trainTickets.getTrainTicketsNumber());
+            if (cursor.getCount()!=0){
+                ContentValues values=new ContentValues();
+                values.put("train_tickets_search_date",trainTickets.getSearchDate());
+                db.update("TrainTickets",values,"train_tickets_number=?",new String[]{trainTickets.getTrainTicketsNumber()});
+            }else {
+                ContentValues values=new ContentValues();
+                values.put("train_tickets_number",trainTickets.getTrainTicketsNumber());
+                values.put("train_tickets_search_date",trainTickets.getSearchDate());
+                db.insert("TrainTickets",null,values);
+            }
         }
     }
 
     //ZipCode
     public void saveZipCodeInformation(ZipCode zipCode){
         if (zipCode!=null){
-            ContentValues values=new ContentValues();
-            values.put("zip_code_number",zipCode.getZipCodeNumber());
-            values.put("zip_code_search_date",zipCode.getSearchDate());
-            db.insert("ZipCode",null,values);
+            Cursor cursor=query("ZipCode",zipCode.getZipCodeNumber());
+            if (cursor.getCount()!=0){
+                ContentValues values=new ContentValues();
+                values.put("zip_code_search_date",zipCode.getSearchDate());
+                db.update("ZipCode",values,"zip_code_number=?",new String[]{zipCode.getZipCodeNumber()});
+            }else {
+                ContentValues values=new ContentValues();
+                values.put("zip_code_number",zipCode.getZipCodeNumber());
+                values.put("zip_code_search_date",zipCode.getSearchDate());
+                db.insert("ZipCode",null,values);
+            }
         }
     }
 
     //ExchangeRate
     public void saveExchangeRateInformation(ExchangeRate exchangeRate){
         if (exchangeRate!=null){
-            ContentValues values=new ContentValues();
-            values.put("exchange_rate",exchangeRate.getExchangeRate());
-            values.put("exchange_rate_search_date",exchangeRate.getSearchDate());
-            db.insert("ExchangeRate",null,values);
+            Cursor cursor=query("ExchangeRate",exchangeRate.getExchangeRate());
+            if (cursor.getCount()!=0){
+                ContentValues values=new ContentValues();
+                values.put("exchange_rate_search_date",exchangeRate.getSearchDate());
+                db.update("ExchangeRate",values,"exchange_rate=?",new String[]{exchangeRate.getExchangeRate()});
+            }else {
+                ContentValues values=new ContentValues();
+                values.put("exchange_rate",exchangeRate.getExchangeRate());
+                values.put("exchange_rate_search_date",exchangeRate.getSearchDate());
+                db.insert("ExchangeRate",null,values);
+            }
         }
     }
 
     //Trademark
     public void saveTrademarkInformation(Trademark trademark){
         if (trademark!=null){
-            ContentValues values=new ContentValues();
-            values.put("trademark",trademark.getTrademark());
-            values.put("trademark_search_date",trademark.getSearchDate());
-            db.insert("Trademark",null,values);
+            Cursor cursor=query("Trademark",trademark.getTrademark());
+            if (cursor.getCount()!=0){
+                ContentValues values=new ContentValues();
+                values.put("trademark_search_date",trademark.getSearchDate());
+                db.update("Trademark",values,"trademark=?",new String[]{trademark.getTrademark()});
+            }else {
+                ContentValues values=new ContentValues();
+                values.put("trademark",trademark.getTrademark());
+                values.put("trademark_search_date",trademark.getSearchDate());
+                db.insert("Trademark",null,values);
+            }
         }
     }
 
     //AttractionsTickets
     public void saveAttractionsTicketsInformation(AttractionsTicketsPrice attractionsTicketsPrice){
         if (attractionsTicketsPrice!=null){
-            ContentValues values=new ContentValues();
-            values.put("attractions_name",attractionsTicketsPrice.getAttractionName());
-            values.put("attractions_search_date",attractionsTicketsPrice.getSearchDate());
-            db.insert("AttractionsTickets",null,values);
+            Cursor cursor=query("AttractionsTickets",attractionsTicketsPrice.getAttractionName());
+            if (cursor.getCount()!=0){
+                ContentValues values=new ContentValues();
+                values.put("attractions_search_date",attractionsTicketsPrice.getSearchDate());
+                db.update("AttractionsTickets",values,"attractions_name=?",new String[]{attractionsTicketsPrice.getAttractionName()});
+            }else {
+                ContentValues values=new ContentValues();
+                values.put("attractions_name",attractionsTicketsPrice.getAttractionName());
+                values.put("attractions_search_date",attractionsTicketsPrice.getSearchDate());
+                db.insert("AttractionsTickets",null,values);
+            }
         }
     }
 
     //PerpetualCalendar
     public void savePerpetualCalendarInformation(PerpetualCalendar perpetualCalendar){
         if (perpetualCalendar!=null){
-            ContentValues values=new ContentValues();
-            values.put("perpetual_calendar",perpetualCalendar.getPerpetualCalendar());
-            values.put("perpetual_calendar_search_date",perpetualCalendar.getSearchDate());
-            db.insert("PerpetualCalendar",null,values);
+            Cursor cursor=query("PerpetualCalendar",perpetualCalendar.getPerpetualCalendar());
+            if (cursor.getCount()!=0){
+                ContentValues values=new ContentValues();
+                values.put("perpetual_calendar_search_date",perpetualCalendar.getSearchDate());
+                db.update("PerpetualCalendar",values,"perpetual_calendar=?",new String[]{perpetualCalendar.getPerpetualCalendar()});
+            }else {
+                ContentValues values=new ContentValues();
+                values.put("perpetual_calendar",perpetualCalendar.getPerpetualCalendar());
+                values.put("perpetual_calendar_search_date",perpetualCalendar.getSearchDate());
+                db.insert("PerpetualCalendar",null,values);
+            }
         }
     }
 
